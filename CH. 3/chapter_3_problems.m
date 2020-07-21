5eb73a86421ff8590a8738cc1f2d1b02fe6a580e
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2020-07-20 21:24:34 -0700

    Update chapter_3_problems.m

--- CH. 3/chapter_3_problems.m
+++ CH. 3/chapter_3_problems.m
@@ -8,9 +8,28 @@
 x = [4 3 2 1 1 2 3 4];
 n = 0:7;
 k = 0:100;
+w = (pi/100)*k;  % calculates the evenly spaced frequencies 
 
 X = dtft(n,x,k);
-% as angle(x) approaches 0.5*pi, phase change dramatically increases
+
+magX = abs(X);  % for graphs
+angX = angle(X);
+realX = real(X);  % divide into real and imaginary parts
+imagX = imag(X);
+
+figure('Color', [1 1 1]);
+sgtitle('Discrete Time Fourier Transform');
+subplot(2,2,1); plot(w*0.5/pi,magX); grid off; axis tight;
+xlabel('frequency in units of pi'); title('Magnitude');
+subplot(2,2,2); plot(w*0.5/pi,realX); grid off; axis tight;
+xlabel('frequency in units of pi'); title('Real part');
+subplot(2,2,3); plot(w*0.5/pi,angX); grid off; axis tight;
+xlabel('frequency in units of pi'); title('Angle');
+subplot(2,2,4); plot(w*0.5/pi,imagX); grid off; axis tight;
+xlabel('frequency in units of pi'); title('Imaginary part');
+
+% the angle graph is odd-symmetric
+
 
 %% P3.2
 % 1). After solving mathematically, the answer is X2(e^jw) = X1(e^jw) + e^-4jw * X1(e^jw)
