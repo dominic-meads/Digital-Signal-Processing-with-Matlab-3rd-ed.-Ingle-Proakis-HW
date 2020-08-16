ffcac0be289b3b5ed8cda9e3340ad6b863cec47e
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2020-08-16 15:04:01 -0700

    Update chapter_3_problems.m
    the stepseq function can be found in the link in README

--- CH. 3/chapter_3_problems.m
+++ CH. 3/chapter_3_problems.m
@@ -39,6 +39,7 @@ xlabel('frequency in units of pi'); title('Imaginary part');
 
 n1 = 0:3;
 x1 = [1 2 2 1];
+figure;
 subplot(2,2,1); stem(n1,x1); title('x1'); axis([0 7 -4 4]); xlabel('sample [n]'); ylabel('x[n]');
 
 n2 = 0:7;
@@ -55,6 +56,40 @@ X2 = dtft(n2,x2,k);
 % check
 w_check = (pi/max(k))*k;
 X2_check = X1 + exp(-4*j*w_check).*X1;  % use element by element multiplication (acts as a scalar)
+figure;
 subplot(2,2,3); stem(w_check/pi,abs(X2_check)); title('My DTFT (analyzed mathematically'); xlabel('Frequency as a fraction of PI'); ylabel('Magnitude');
 subplot(2,2,4); stem(w_check/pi,abs(X2)); title('MATLAB computed DTFT (computed)'); xlabel('Frequency as a fraction of PI'); ylabel('Magnitude');
 error = max(abs(X2 - X2_check))  % nominal error ~0%
+
+
+%% P3.3
+
+% 1). Find the DTFT of x[n] = 2(0.5)^n * u[n+2]
+
+%     After solving analytically, X(e^jw) = 8e^(2jw) + 4e^(jw) + (2e^jw)/(e^jw - 0.5)
+
+%     The shift in the unit step function causes the range for the
+%     summation to be -2 <= n <= inf. A geometric series (3rd term in the
+%     X(e^jw) equation above), is only defined a(r)^n when |r| < 1, or for
+%     all n > 0. However, since our n includes negative samples, the
+%     equation must account for those (when n < 0, |r| >= 1). Once all 
+%     n < 0 terms are calculated, a geometric series for n < 0 can be used 
+%     since |r| is now < 1.    
+
+n = -2:200;                 % the sequence is zero for all n < -2 (due to shift)
+u = stepseq(-2,-2,200);     % generate shifted unit step function
+x = 2 * power(0.5,n) .* u;  % use element by element mult for each sample
+k = 0:500;
+w = k*pi/max(k);
+
+X = dtft(n,x,k);
+
+figure;
+subplot(2,2,1); plot(w/pi,abs(X)); title('MATLAB Answer (Mag)'); xlabel('w as a fraction of pi'); ylabel('Magnitude'); axis([0 1 0 20]);
+subplot(2,2,2); plot(w/pi,angle(X)*180/pi); title('MATLAB Answer (Ang)'); xlabel('w as a fraction of pi'); ylabel('Phase (degrees)'); axis([0 1 -200 200]);
+
+% check answer 
+X_check = 8*exp(2*j*w) + 4*exp(j*w) + (2*exp(j*w))./(exp(j*w)-0.5);  % my answer calculated analytically
+subplot(2,2,3); plot(w/pi,abs(X_check)); title('My Answer (Mag)'); xlabel('w as a fraction of pi'); ylabel('Magnitude'); axis([0 1 0 20]);
+subplot(2,2,4); plot(w/pi,angle(X_check)*180/pi); title('My Answer (Ang)'); xlabel('w as a fraction of pi'); ylabel('Phase (degrees)'); axis([0 1 -200 200]);
+error = max(abs(X-X_check))  % nominal error ~= 0%;
