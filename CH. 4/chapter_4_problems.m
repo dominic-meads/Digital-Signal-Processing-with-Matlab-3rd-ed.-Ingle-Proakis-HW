a5c22f46cb650a609205a057e199545484c2ba30
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2020-10-15 19:17:31 -0700

    Update chapter_4_problems.m

--- CH. 4/chapter_4_problems.m
+++ CH. 4/chapter_4_problems.m
@@ -37,3 +37,54 @@ x3check = ((n3+1).*(3.^n3)).* x3checkpart1
 error1 = max(abs(x3check-x3))  % no error, the z-transform above is the same
 
 %% P4.2
+% Given x(n) = (0.9)^n * cos(pi*n/4) * u(n)
+% and
+%               [  x(n/2), n = 0, +-2, +-4, ...
+%       y(n) = [  0,      otherwise
+%
+% 1). show Y(z) = X(z^2)
+%          n  = [-inf, ... -2,    -1,    0,    1,    2,  ... +inf]
+%        x(n) = [-inf, ...x(-2), x(-1), x(0), x(1), x(2), ...+inf]
+%        X(z) = [-inf, ...x(-2)*z^2, x(-1)*z^1, x(0), x(1)*z^-1, x(2)*z^-2, ...+inf]
+% 
+%        y(n) = [-inf, ...x(-1),  0  , x(0) ,  0  , x(1), ...+inf]
+%        Y(z) = [-inf, ...x(-1)*z^2, x(0), x(1)*z^-2, ...+int] = X(z^2)
+%
+% 2). Determine Y(z)
+%
+%                             1 - 0.6346z^-2
+%        Y(z) = X(z^2) = -------------------------   |z| > 0.9
+%                        1 - 1.2728z^-2 + 0.81z^-4
+%
+% 3). Verify Y(z)
+%      (i will only be using non-negative n because the sequence is causal)
+% create x(n)
+n = 0:100; 
+x = ((0.9).^n).*cos(pi*n/4);
+
+% create y(n)
+y = zeros(1,101);  
+for k = 1:101
+    if (mod(k,2)==0)  % n is even
+       y(k) = x(k/2);
+    else 
+        y(k) = 0;
+    end
+end
+y = circshift(y,-1);  
+% the reason I have to do the shift is to make y match up with the value of
+% n, not the index number (which cannot be zero in MATLAB)
+
+% verify calculated X(z), take inverse z-transform and check with x 
+bx = [1 -0.6364 0];
+ax = [1 -1.2728 0.81];
+[deltax,nx] = impseq(0,0,100);
+x_check = filter(bx,ax,deltax);
+error_x = max(abs(x_check-x))
+
+% % verify calculated Y(z), take inverse z-transform and check with y 
+by = [1 0 -0.6364 0 0];
+ay = [1 0 -1.2728 0 0.81];
+[deltay,ny] = impseq(0,0,100);
+y_check = filter(by,ay,deltay);
+error_y = max(abs(y_check-y))
